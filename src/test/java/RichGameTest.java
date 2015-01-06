30b733c5141b4af23daf28af4ee6aaa75db86d11
Cui Liqiang <cui.liqiang@gmail.com>
2015-01-06 10:39:02 +0800

    migrate to maven

--- src/test/java/RichGameTest.java
+++ src/test/java/RichGameTest.java
@@ -4,11 +4,13 @@ import org.junit.Test;
 
 import java.io.*;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.endsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.endsWith;
 
 public class RichGameTest {
+    public static final String EOF = "\26";
+//    Dice mockDice = mock(Dice.class);
     RichGame richGame = new RichGame();
     OutputStream outputStream = new ByteArrayOutputStream();
     PrintStream out = new PrintStream(outputStream);
@@ -25,10 +27,9 @@ public class RichGameTest {
 
     @Test
     public void should_be_able_to_draw_map() {
-        String inputString = "\26";
-        InputStream in = new StringBufferInputStream(inputString);
-        System.setIn(in);
+        setInput(EOF);
         richGame.run();
+
         String map = "S0000000000000Y0000000000000Z\n" +
                 "$                           0\n" +
                 "$                           0\n" +
@@ -42,13 +43,31 @@ public class RichGameTest {
         assertThat(actualOutput, containsString(map));
     }
 
-    @Test
-    public void should_be_able_to_quit() {
-        String inputString = "quit\n";
+    private void setInput(String inputString) {
         InputStream in = new StringBufferInputStream(inputString);
         System.setIn(in);
+    }
+
+    @Test
+    public void should_be_able_to_quit() {
+        setInput("quit\n");
         richGame.run();
 
         assertThat(outputStream.toString(), endsWith("游戏结束\n"));
+    }
+
+    @Test
+    public void should_be_able_to_work_for_single_player() {
+        setInput("work\n" + EOF);
+        richGame.run();
+
+        String mapWithPlayer = "S0000000000000Y0000000000000Z\n" +
+                               "$                           0\n" +
+                               "$                           0\n" +
+                               "$                           0\n" +
+                               "$                           0\n" +
+                               "$                           0\n" +
+                               "$                           0\n" +
+                               "J0000000000000T0000000000000Z\n";
     }
 }
